8ef7d4f6c824447bb61de5439ed922032638adfb
Javier Moran <javiermoran7@gmail.com>
2019-02-13 11:41:45 +0100

    This Is Prada

---
+++ Collections/src/Prada.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Iterator;
+
+public class Prada {
+
+	public static void main(String[] args) {
+
+		Random r = new Random();
+
+		List<Integer> lista = new ArrayList<Integer>();
+		for (int i = 0; i < 100; i++) {
+			lista.add(r.nextInt(100) + 1);
+		}
+		mostrarColeccion(lista);
+
+		HashSet<Integer> set = new HashSet<Integer>();
+		mostrarColeccion(set);
+	}
+
+	static void mostrarColeccion(Collection<Integer> c) {
+		Iterator<Integer> i = c.iterator();
+		while (i.hasNext()) {
+			Integer numero = i.next();
+			if (i.hasNext())
+				System.out.print(numero + ", ");
+		}
+		System.out.println();
+		System.out.println("Numero de numeros hallados " + c.size());
+	}
+}
